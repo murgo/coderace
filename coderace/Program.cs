127099408e5e046af2cbccae6843c9f1086301ae
Lauri Härsilä <murgo@iki.fi>
2017-04-06 17:32:10 +0300

    Initial commit, placeholder stuff

---
+++ coderace/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace coderace
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
