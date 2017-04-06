34b3d09a28aec8d8c7e5c2d8aa781984f0cee348
Lauri Härsilä <murgo@iki.fi>
2017-04-06 17:39:08 +0300

    Changed hello

--- coderace/Program.cs
+++ coderace/Program.cs
@@ -6,7 +6,7 @@ namespace coderace
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("💣💀 wroom wroom");
         }
     }
 }
